3674296ce8a20a6230beac6e36c27d6ee809dd2d
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2024-08-05 18:10:02 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.reminder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:reminder/app/app.dart';
+import 'package:reminder/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
